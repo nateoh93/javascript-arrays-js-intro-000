d4695188df38d9182039683327b3b0c5c82266c9
Nathan Oh <nathanoh93@gmail.com>
2020-02-22 21:57:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray () {
   var addElementToBeginningOfArray = ["array", "element"]
 }
-function addElementToBeginningOfArray
+function addElementToBeginningOfArray ()
