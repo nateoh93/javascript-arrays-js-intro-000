8e0a65d5b45890ee026fe359fd577be8d515bb9b
Nathan Oh <nathanoh93@gmail.com>
2020-02-22 23:04:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  [element,...array]
-  return array
+  return [element,...array]
 }
 //destructivelyAddElementToBeginningOfArray(["one", "two", "three"], "four")
 function destructivelyAddElementToBeginningOfArray(array, element) {
