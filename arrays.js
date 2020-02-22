1382f71f622d0b4d1c5e039c2d024134740470be
Nathan Oh <nathanoh93@gmail.com>
2020-02-22 23:08:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
   }
 
 function accessElementInArray (array, index) {
-  return array(element)
+  return array(index)
 }
 
 function destructivelyRemoveElementFromBeginningOfArray() {
