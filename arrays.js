a06c14279aaed50cdc0f3d6a6ffa5ea7271fe456
Nathan Oh <nathanoh93@gmail.com>
2020-02-22 21:54:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,7 @@ function addElementToBeginningOfArray () {
 
 }
 ["foo", 1, ...addElementToBeginningOfArray]
+function destructivelyAddElementToBeginningOfArray () {
+  var destructivelyAddElementToBeginningOfArray = ["array", "element"]
+  destructivelyAddElementToBeginningOfArray = ["destroy", ...destructivelyAddElementToBeginningOfArray]
+}
